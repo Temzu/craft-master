5e0014814a09750dcfbb77ade6bb27d2ebe3c7a0
temzu <artem-zubkov-dev@inbox.ru>
2021-08-23 20:18:55 +0300

    Initial commit

---
+++ src/main/java/com/gb/agile/craft_master/CraftMasterApplication.java
@@ -0,0 +1,13 @@
+package com.gb.agile.craft_master;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraftMasterApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CraftMasterApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/gb/agile/craft_master/CraftMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gb.agile.craft_master;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CraftMasterApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
