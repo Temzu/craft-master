79098b23fc18d69db0213e03c106930f7c0a5820
Ekaterina Malygina <katty2007v@yandex.ru>
2021-09-22 00:07:01 +0300

    Add credentials into UserInfo

--- src/main/java/com/gb/agile/craft_master/model/dto/CredentialDto.java
+++ src/main/java/com/gb/agile/craft_master/model/dto/CredentialDto.java
@@ -1,5 +1,6 @@
 package com.gb.agile.craft_master.model.dto;
 
+import com.gb.agile.craft_master.model.Credential;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -9,4 +10,10 @@ public class CredentialDto {
     private String code;
     private String value;
     private String name;
+
+    public CredentialDto(Credential credential) {
+        this.code = credential.getCode();
+        this.value = credential.getValue();
+        this.name = credential.getName();
+    }
 }
