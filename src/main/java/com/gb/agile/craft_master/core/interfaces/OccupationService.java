da0e42988480d68fd38aa76bc2328e0251badbec
Andrey <sozykin@yandex.ru>
2021-10-05 09:59:34 +0300

    - переименование таблицы service - в occupation - создание сервиса работ (occupation) - разрешение конфликтов мержа - удалил Service - замена Integer на Long

---
+++ src/main/java/com/gb/agile/craft_master/core/interfaces/OccupationService.java
@@ -0,0 +1,11 @@
+package com.gb.agile.craft_master.core.interfaces;
+
+import com.gb.agile.craft_master.model.entities.Occupation;
+
+import java.util.List;
+
+public interface OccupationService {
+
+    List<Occupation> getAll();
+
+}
