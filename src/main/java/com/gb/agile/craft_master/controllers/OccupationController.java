48e726c20de1bb2434b15289905327678eaa476c
Temzu <40913z@mail.ru>
2021-10-13 17:57:10 +0300

    added preauthorize in OccupationController

--- src/main/java/com/gb/agile/craft_master/controllers/OccupationController.java
+++ src/main/java/com/gb/agile/craft_master/controllers/OccupationController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/occupations")
 @RequiredArgsConstructor
+@PreAuthorize("isAuthenticated()")
 public class OccupationController {
 
 private final OccupationService occupationService;
